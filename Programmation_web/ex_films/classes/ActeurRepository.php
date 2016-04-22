d444f4e1bca20888abe767982be68dad655bdaec
Martin Bolot <bolotmartin@gmail.com>
2016-04-22 16:29:32 +0200

    update projet php

--- Programmation_web/ex_films/classes/ActeurRepository.php
+++ Programmation_web/ex_films/classes/ActeurRepository.php
@@ -6,14 +6,14 @@
         */
         public function __construct(){
             $this->pdoConnect = connectDb();
-            $query = $this->pdoConnect->prepare("select id_acteur,nom_acteur,prenom_acteur from acteurs");
+            $query = $this->pdoConnect->prepare("select id_acteur,nom,prenom from acteurs");
             $query->execute();
             $acteurs = $query->fetchAll();
 
             foreach($acteurs as $acteur){
                 $acteurObject = new Acteur(
-                    $acteur['nom_acteur'],
-                    $acteur['prenom_acteur']
+                    $acteur['nom'],
+                    $acteur['prenom']
                 );
                 $acteurObject->setId($acteur['id_acteur']);
                 array_push($this->acteurArray,$acteurObject);
@@ -30,15 +30,15 @@
         *       => 2 if an error happend
         */
         public function save($acteur){
-			$testQuery = $this->pdoConnect->prepare("select count(*) as nb from acteurs where nom_acteur=? and prenom_acteur=?");
-			$testQuery->execute(array($_POST["nom_acteur"],$_POST["prenom_acteur"]));
+			$testQuery = $this->pdoConnect->prepare("select count(*) as nb from acteurs where nom=? and prenom=?");
+			$testQuery->execute(array($acteur->getNom(),$acteur->getPrenom()));
 
 			$data = $testQuery->fetch();
 
 			if($data["nb"] > 0){
 				return 1;
             }else{
-    			$query = $this->pdoConnect->prepare("insert into p1506898.acteurs(nom_acteur,prenom_acteur) values(:nom,:prenom)");
+    			$query = $this->pdoConnect->prepare("insert into acteurs(nom,prenom) values(:nom,:prenom)");
     			$insert = $query->execute(array(
     				'nom' => $acteur->getNom(),
     				'prenom' => $acteur->getPrenom()
