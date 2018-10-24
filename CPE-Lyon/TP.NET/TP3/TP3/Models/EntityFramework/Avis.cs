dbca49d5b78b646019df4c70cecb0f01ebb460cf
Martin BOLOT <martin.bolot@cpe.fr>
2018-10-24 12:06:02 +0200

    ajout TP3

---
+++ CPE-Lyon/TP.NET/TP3/TP3/Models/EntityFramework/Avis.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP3.Models.EntityFramework
+{
+    [Table("T_E_AVIS_AVI")]
+    public class Avis
+    {
+        public Avis()
+        {
+            CompteAvis = new Compte();
+            FilmAvis = new Film();
+        }
+        
+        [Column("CPT_ID")]
+        [ForeignKey("Compte")]
+        public int CompteId { get; set; }
+        
+        [Column("FLM_ID")]
+        [ForeignKey("Film")]
+        public int FilmId { get; set; }
+
+        [Column("AVI_DATE")]
+        [Required]
+        public DateTime DateAvis { get; set; }
+
+        [Column("AVI_TITRE")]
+        [Required]
+        [StringLength(100)]
+        public string Titre { get; set; }
+
+        [Column("AVI_DETAIL")]
+        [Required]
+        [StringLength(2000)]
+        public string Detail { get; set; }
+
+        [Column("AVI_NOTE", TypeName="numeric(1)")]
+        [Required]
+        public decimal Note { get; set; }
+
+        // Propriétés de navigation vers Compte et Film.
+        // Les nommer CompteAvis et FilmAvis.
+        [InverseProperty("AvisCompte")]
+        public Compte CompteAvis { get; set; }
+
+        [InverseProperty("AvisFilm")]
+        public Film FilmAvis { get; set; }
+    }
+}
---
+++ CPE-Lyon/TP.NET/TP3/TP3/Models/EntityFramework/Film.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP3.Models.EntityFramework
+{
+    [Table("T_E_FILM_FLM")]
+    public class Film
+    {
+        public Film()
+        {
+            AvisFilm = new HashSet<Avis>();
+            FavoriFilm = new HashSet<Favori>();
+        }
+
+        [Column("FLM_ID")]
+        public int FilmId { get; set; }
+
+        [Column("FLM_TITRE")]
+        [Required]
+        [StringLength(100)]
+        public string Titre { get; set; }
+
+        [Column("FLM_SYNOPSIS")]
+        [StringLength(500)]
+        public string Synopsis { get; set; }
+
+        [Column("FLM_DATEPARUTION")]
+        public DateTime DateParution { get; set; }
+
+        [Column("FLM_DUREE", TypeName="numeric(3,0)")]
+        [Required]
+        public decimal Duree { get; set; }
+
+        [Column("FLM_GENRE")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+
+        [Column("FLM_URLPHOTO")]
+        [StringLength(200)]
+        public string UrlPhoto { get; set; }
+
+        // Propriétés de navigation vers Avis et Favori
+        [InverseProperty("FilmAvis")]
+        public ICollection<Avis> AvisFilm { get; set; }
+        [InverseProperty("FilmFavori")]
+        public ICollection<Favori> FavoriFilm { get; set; }
+
+    }
+}
