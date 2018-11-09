d54e43ee79953182ea8e64ed977aaf8599a45a78
CPE <cpe@localhost.localdomain>
2018-11-09 17:36:19 +0100

    fin td

--- CPE-Lyon/React/my-react-app/src/components/MiddleSide/MiddleSide.js
+++ CPE-Lyon/React/my-react-app/src/components/MiddleSide/MiddleSide.js
@@ -2,11 +2,6 @@ import React, { Component } from 'react';
 import Part from "../Part/Part";
 
 class MiddleSide extends Component {
-  constructor(props) {
-    super(props);
-    this.handlePartClick = this.handlePartClick.bind(this);
-  }
-
   render() {
     if(this.props.parts) {
       return this.props.parts.map((part, index) => {
