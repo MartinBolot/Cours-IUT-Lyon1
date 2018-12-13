5ae397d38934d3e448e7da3407a1a72a487eb549
Martin Bolot <martin@martinbolot.com>
2018-12-13 20:00:49 +0100

    avancee bd

--- CPE-Lyon/Big Data/get_violation_location.scala
+++ CPE-Lyon/Big Data/get_violation_location.scala
@@ -1,4 +1,20 @@
 /* spark-shell --master yarn --conf spark.ui.port=4050 */
 
-val resultsDF = spark.sql("SELECT COUNT(1) FROM refine1_violations")
-resultsDF.show()
+/* open csv data file */
+val df = spark.read.format("csv").option("header", "true").load("extract_violations.csv")
+// df.show()
+
+val locations = df.select($"House Number", $"Street Name", $"Intersecting Street")
+
+/* map columns to a function */
+val findLocation = udf((houseNumber: String, streetName: String, intersectingStreet: String) => {
+	// TODO get location from API
+	houseNumber + streetName + intersectingStreet
+})
+
+val newDf = locations.withColumn("location", findLocation(
+	$"House Number", $"Street Name", $"Intersecting Street"
+))
+
+/* write output csv file */
+df.coalesce(1).write.csv("csv_export")
